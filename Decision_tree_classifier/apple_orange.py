714060224cf0cee31630fa98e7e84105c1d56f98
theashwanisingla <34922135+theashwanisingla@users.noreply.github.com>
2020-01-24 00:54:36 +0530

    Update apple_orange.py

--- Decision_tree_classifier/apple_orange.py
+++ Decision_tree_classifier/apple_orange.py
@@ -4,11 +4,7 @@ feature = [[190,0],[200,0],[210,1],[220,1]]
 label = ["Apple","Apple","orange","orange"]
 
 clf = tree.DecisionTreeClassifier()
-
-
 trained = clf.fit(feature,label)
-
-
 result = trained.predict([[200,1]])
 
 print (result)
