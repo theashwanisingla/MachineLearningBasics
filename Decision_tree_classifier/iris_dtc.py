ade6b72c6508f8265a226ecf3bda4af0f1de4f16
theashwanisingla <34922135+theashwanisingla@users.noreply.github.com>
2020-01-24 00:52:59 +0530

    Update iris_dtc.py

--- Decision_tree_classifier/iris_dtc.py
+++ Decision_tree_classifier/iris_dtc.py
@@ -1,12 +1,19 @@
-from sklearn import tree
 import numpy
+from sklearn import tree
 from sklearn.datasets import load_iris
-new_data = numpy.delete(load_iris().data,[0,50,100],axis=0)
-new_target = numpy.delete(load_iris().target,[0,50,100])
-check_data = load_iris().data[[0,50,100]]
-check_target = load_iris().target[[0,50,100]]
-clf = tree.DecissionTreeClassifer()
-trained = clf.fit(new_data,new_target)
-result = trained.predict(check_data[[0]])
-print (result)
 
+#creating object for dataset
+iris = load_iris()
+#creating object for classifier algorithm
+clf = tree.DecisionTreeClassifier()
+
+#creating a training data set
+training_data = numpy.delete(iris.data,[0,50,100],axis=0) #training_Data/feature
+training_target = numpy.delete(iris.target,[0,50,100])    #training_target/label
+
+test_data = iris.data[[0,50,100]]
+
+trained = clf.fit(training_data,training_target)
+result = trained.predict(test_data[[0]])
+
+print (iris.target_names[result[0]])
